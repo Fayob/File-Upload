299110e93d760d0b38a38ec57c63216a98c4b8ed
= <adedejifavour6@gmail.com>
2022-03-30 15:54:50 +0400

    File upload setup

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 5000;
+// database
+const connectDB = require("./db/connect");
 
-app.listen(port, console.log(`The Server is listening on PORT ${port}...`));
+// error handler
+const notFoundMiddleWare = require("./middleware/not_found");
+const errorHandlerMiddleWare = require("./middleware/error-handler");
+
+app.get("/", (req, res) => {
+  res.send("<h1> File Upload Starts here </h1>");
+});
+
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleWare);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`The Server is listening on PORT ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
